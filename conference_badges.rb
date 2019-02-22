19b4719bba837609bdaf06b7f7e20c2f22d553a5
lieby weintraub <liebahinda@gmail.com>
2019-02-22 01:53:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,16 +1,19 @@
-# Write your code here.
+
+ 
+ # Write your code here.
 def badge_maker(name)
-return "Hello, my name is #{name}."
+ "Hello, my name is #{name}."
 end
 
- def batch_badge_creator(name)
-  name.collect do |name|
+ def batch_badge_creator(atendees)
+  atendees.collect do |name|
     badge_maker(name)
   end
 end
 
+
  def assign_rooms(speakers)
-  speakers.collect.each_with_index do |speaker, index|
+  speakers.collect. each_with_index do |speaker, index|
      "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
   end
 end
